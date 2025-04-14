14dff3177fde3f3ee416b14f174302e5704562d9
Laurentino Jaimes <ljaimes92@gmail.com>
2025-04-14 18:52:01 -0400

    got some html set up, game board and some game logic are set up. working on player turn.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+const Gameboard = (() => {
+    let grid = ["", "", "", "", "", "", "", "", ""];
+    const getGrid = () => grid;
+
+    const resetGrid = () => {
+        grid = ["", "", "", "", "", "", "", "", ""];
+    };
+    return { getGrid, setBlock, resetGrid };
+});
+
+
+const Player = (name, marker) => {
+    return { name, marker };
+};
+
+console.log(Player)
+
+const gameLogic = (() => {
+    const player1 = Player("Player 1", "x");
+    const player2 = Player("player 2", "o");
+    let currentPlayer = player1;
+
+    const changePlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const playTurn = (index) => {
+        //put user name and x or o in the grid/ object
+        //
+    }
+}); 
